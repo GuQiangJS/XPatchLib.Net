f484b8a031a614d80e6bad6d9bd2b36236406196
GuQiangJS <GuQiangJS@hotmail.com>
2017-05-01 18:07:17 +0800

    增加单元测试

--- src/XPatchLib.UnitTest/ForXml/TestXmlSerializer.cs
+++ src/XPatchLib.UnitTest/ForXml/TestXmlSerializer.cs
@@ -2,6 +2,7 @@
 // Licensed under the LGPL-3.0 license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
@@ -14,6 +15,14 @@ namespace XPatchLib.UnitTest.ForXml
     [TestClass]
     public class TestXmlSerializer
     {
+        public class Account
+        {
+            public string Email { get; set; }
+            public bool Active { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public IList<string> Roles { get; set; }
+        }
+
         #region Private Fields
 
         private const string ChangedContext = @"<?xml version=""1.0"" encoding=""utf-8""?>
@@ -38,7 +47,7 @@ namespace XPatchLib.UnitTest.ForXml
         {
             get
             {
-                BookClass revObj = BookClass.GetSampleInstance();
+                var revObj = BookClass.GetSampleInstance();
                 revObj.Author.Name = newAuthorName;
                 return revObj;
             }
@@ -73,12 +82,12 @@ namespace XPatchLib.UnitTest.ForXml
         [Description("测试Serializer中参数类型为 XmlTextReader 和 XmlTextWriter 的Divide和Combine方法。测试对象包含 XmlIgnoreAttribute")]
         public void TestXmlSerializerStreamDivideAndCombineForIgnoreAttribute()
         {
-            XmlIgnoreClass c1 = new XmlIgnoreClass() {A = "A", B = "B"};
-            XmlIgnoreClass c2 = new XmlIgnoreClass() { A = "C", B = "D" };
+            var c1 = new XmlIgnoreClass {A = "A", B = "B"};
+            var c2 = new XmlIgnoreClass {A = "C", B = "D"};
             //因为属性A不参与序列化，所以应该还是原值
-            XmlIgnoreClass c3 = new XmlIgnoreClass() { A = "A", B = "D" };
+            var c3 = new XmlIgnoreClass {A = "A", B = "D"};
 
-            string changedContext = @"<?xml version=""1.0"" encoding=""utf-8""?>
+            var changedContext = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <XmlIgnoreClass>
   <B>D</B>
 </XmlIgnoreClass>";
@@ -87,7 +96,7 @@ namespace XPatchLib.UnitTest.ForXml
             {
                 using (var writer = TestHelper.CreateWriter(stream, TestHelper.DocumentSetting))
                 {
-                    Serializer serializer = new Serializer(typeof(XmlIgnoreClass));
+                    var serializer = new Serializer(typeof(XmlIgnoreClass));
                     serializer.Divide(writer, c1, c2);
                     var context = UnitTest.TestHelper.StreamToString(stream);
                     Assert.AreEqual(changedContext, context);
@@ -97,10 +106,10 @@ namespace XPatchLib.UnitTest.ForXml
             {
                 using (var writer = TestHelper.CreateWriter(stream, TestHelper.DocumentSetting))
                 {
-                    Serializer serializer = new Serializer(typeof(XmlIgnoreClass));
+                    var serializer = new Serializer(typeof(XmlIgnoreClass));
                     serializer.Divide(writer, c1, c2);
                     stream.Position = 0;
-                    using (XmlTextReader reader = new XmlTextReader(XmlReader.Create(stream)))
+                    using (var reader = new XmlTextReader(XmlReader.Create(stream)))
                     {
                         var changedObj = serializer.Combine(reader, c1) as XmlIgnoreClass;
                         Assert.AreEqual(c3, changedObj);
@@ -113,7 +122,7 @@ namespace XPatchLib.UnitTest.ForXml
         [Description("测试Serializer中参数类型为 XmlTextReader 和 XmlTextWriter 的Divide和Combine方法")]
         public void TestXmlSerializerStreamDivideAndCombine()
         {
-            Serializer serializer = new Serializer(typeof(BookClass));
+            var serializer = new Serializer(typeof(BookClass));
             using (var stream = new MemoryStream())
             {
                 using (var writer = TestHelper.CreateWriter(stream, TestHelper.DocumentSetting))
@@ -130,7 +139,7 @@ namespace XPatchLib.UnitTest.ForXml
                 {
                     serializer.Divide(writer, OriObject, RevObject);
                     stream.Position = 0;
-                    using (XmlTextReader reader = new XmlTextReader(XmlReader.Create(stream)))
+                    using (var reader = new XmlTextReader(XmlReader.Create(stream)))
                     {
                         var changedObj = serializer.Combine(reader, OriObject) as BookClass;
                         Assert.AreEqual(RevObject, changedObj);
@@ -149,7 +158,7 @@ namespace XPatchLib.UnitTest.ForXml
             ;
             settings.Encoding = Encoding.ASCII;
 
-            Serializer serializer = new Serializer(typeof(BookClass));
+            var serializer = new Serializer(typeof(BookClass));
             using (var stream = new MemoryStream())
             {
                 using (var writer = TestHelper.CreateWriter(stream, settings))
@@ -166,7 +175,7 @@ namespace XPatchLib.UnitTest.ForXml
                 {
                     serializer.Divide(writer, OriObject, RevObject);
                     stream.Position = 0;
-                    using (XmlTextReader reader = new XmlTextReader(XmlReader.Create(stream)))
+                    using (var reader = new XmlTextReader(XmlReader.Create(stream)))
                     {
                         var changedObj = serializer.Combine(reader, OriObject) as BookClass;
                         Assert.AreEqual(RevObject, changedObj);
@@ -241,6 +250,54 @@ namespace XPatchLib.UnitTest.ForXml
         //        }
         //    }
         //}
+
+        [TestMethod]
+        public void SimpleTestXmlSerializer()
+        {
+            const string context = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<Account>
+  <Active>true</Active>
+  <CreatedDate>2013-01-20T00:00:00Z</CreatedDate>
+  <Email>xpatchlib@example.com</Email>
+  <Roles>
+    <String Action=""Add"">User</String>
+    <String Action=""Add"">Admin</String>
+  </Roles>
+</Account>";
+
+            var account1 = new Account();
+
+            var account2 = new Account
+            {
+                Email = "xpatchlib@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                {
+                    "User",
+                    "Admin"
+                }
+            };
+
+            var serializer = new Serializer(typeof(Account));
+            using (var stream = new MemoryStream())
+            {
+                var settings = new XmlWriterSettings
+                {
+                    ConformanceLevel = ConformanceLevel.Document,
+                    Indent = true,
+                    Encoding = Encoding.UTF8,
+                    OmitXmlDeclaration = false
+                };
+                using (var writer = TestHelper.CreateWriter(stream,settings))
+                {
+                    serializer.Divide(writer, account1, account2);
+                    var result = UnitTest.TestHelper.StreamToString(stream);
+                    Debug.WriteLine(context);
+                    Assert.AreEqual(context,result);
+                }
+            }
+        }
 
         #endregion Public Methods
     }
